4c0e5bbc41b429eda2cf1ab6120c33eb3a8c1af5
Ris Adams <chris@tuesdaybegins.com>
2019-08-12 09:04:29 -0400

    Adding format specifier, glob input dirs

--- excel-shred.py
+++ excel-shred.py
@@ -5,20 +5,24 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'], ignore_unknown_optio
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.argument('files', nargs=-1, type=click.Path(exists=True))
-def cli(files):
+@click.option('--format','-f', default='json', type=click.Choice(['json', 'csv']))
+@click.argument('input_dirs', nargs=-1, type=click.Path(exists=True))
+def cli(format, input_dirs):
     """
     Open an Excel workbook, and convert all sheets to json datasets
-    :param files: an excel workbook filename
+    :param input_dirs: one or more directory paths containing excel workbooks
 
     Example:
     \b
-    excel-shred filename
+    excel-shred input_dir_a [input_dir_b]
     """
 
-    for file in files:
-        print(f'excel-shred:  operating file: {file}')
-        exutil.openSheets(file)
+    print(f"Excel shredding all files to {format}")
+
+    for path in input_dirs:
+        for file in exutil.open_dir(path):
+            print(f'\tShredding : {file}')
+            exutil.shred_sheets(file, format)
 
 
 cli()
--- exutil/__init__.py
+++ exutil/__init__.py
@@ -1,15 +1,41 @@
 import os
+from pathlib import Path
+
 import pandas as pd
 
 
-def file_split( input_file):
+def extract_dir_name(input_file):
+    """
+    creates a directory path based on the specified file name
+    :param input_file:
+    :return:
+    """
     s = input_file.split('.')
-    name = '.'.join(s[:-1]) # extract directory name
+    name = '.'.join(s[:-1])
     return name
 
 
-def openSheets( input_file):
-    name = file_split(input_file)
+def open_dir(input_path):
+    """
+    Opens the specified input path and returns any located excel file
+    :param input_path:
+    :return:
+    """
+    for file in Path(input_path).glob('**/*.xls'):
+        yield file
+
+    for file in Path(input_path).glob('**/*.xlsx'):
+        yield file
+
+
+def shred_sheets(input_file, _format):
+    """
+    Opens an excel workbook, and converts all sheets to a new file of the specified format
+    :param input_file:
+    :param _format:
+    :return:
+    """
+    name = extract_dir_name(input_file) + f"_{format}"
     try:
         os.makedirs(name)
     except:
@@ -17,8 +43,13 @@ def openSheets( input_file):
 
     wb = pd.ExcelFile(input_file)
     for ws in wb.sheet_names:
-        print(ws+'.json', 'Done!')
+        print(ws + '.' + _format, 'Done!')
         data = pd.read_excel(input_file, sheet_name=ws)
-        new_file = os.path.join(name, ws + '.json')
-        data.to_json(new_file, orient="records")
+        new_file = os.path.join(name, ws + '.' + _format)
+        if _format == 'json':
+            data.to_json(new_file, orient="records")
+        elif _format == 'csv':
+            data.to_csv(new_file)
+        else:
+            raise AssertionError(f"Invalid format {_format}")
     print('Complete')
